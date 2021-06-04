6e8335151ea19145598ebc2b4338228535b545de
Maided <maided.h.p90@gmail.com>
2021-06-03 23:02:02 -0500

    estoy añadiendo autenticacion para usuario, k mocio:3

--- components/user/model.js
+++ components/user/model.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const mySchema = new Schema({
-  name: String
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    required: true
+  }
 })
 
 const model = mongoose.model('users', mySchema)
