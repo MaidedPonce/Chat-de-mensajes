97692599410374b7fd595b9d698258a1d642522e
Maided Ponce <66448852+MaidedPonce@users.noreply.github.com>
2021-06-30 22:13:28 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  dbUrl: process.env.DB_URL || 'mongodb+srv://maidedhp:huachikk201@cluster0.3xupg.mongodb.net/test',
-  port: process.env.PORT || 3000,
-  host: process.env.HOST || 'http://localhost',
-  publicRoute: process.env.PUBLIC_ROUTE || '/app',
-  filesRoute: process.env.FILES_ROUTE || 'files'
-}
-
-module.exports = { config }
